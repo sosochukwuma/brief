b2f1c602d9e0902324416b59c23f8b919537260e
sosochukwuma <bossan.sb@gmail.com>
2021-05-05 09:46:29 +0100

    still debugging

--- lib/main.dart
+++ lib/main.dart
@@ -18,132 +18,135 @@ class Home extends StatelessWidget {
           // title: Text('Port4me')
         ),
         body: Center(
-          child: Column(
-            children: <Widget>[
-              SizedBox(
-                height: 10,
-              ),
-              Text(
-                'Flutter Developer',
-                style: TextStyle(
-                  fontSize: 32,
-                  fontWeight: FontWeight.w900,
-                  color: Colors.indigoAccent,
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                SizedBox(
+                  height: 20,
                 ),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              CircleAvatar(
-                radius: 100,
-                backgroundImage: AssetImage('img/sndmn.png'),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              Text(
-                'My name is Sochima',
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.w700),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  children: <Widget>[
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Icon(
-                          Icons.mail_outline,
-                          size: 50,
-                        ),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        Text(
-                          'bossan.sb@gmail.com',
-                          style: TextStyle(fontSize: 24),
-                        )
-                      ],
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Icon(
-                          Icons.location_on,
-                          size: 50,
-                        ),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        Text(
-                          'Nigerian',
-                          style: TextStyle(fontSize: 24),
-                        )
-                      ],
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Icon(
-                          Icons.home,
-                          size: 50,
-                        ),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        Text(
-                          'Flutter, Kotlin, Java',
-                          style: TextStyle(fontSize: 24),
-                        )
-                      ],
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Icon(
-                          Icons.account_circle,
-                          size: 50,
-                        ),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        Text(
-                          'Experience Deisigner, Software Developer, Entreprenuer',
-                          style: TextStyle(fontSize: 24),
-                        )
-                      ],
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                  ],
-                ),
-              ),
-              MaterialButton(
-                onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (content) {
-                    return Details();
-                  }));
-                },
-                child: Padding(
-                  padding: EdgeInsets.only(top: 10, bottom: 10),
-                  child: Text(
-                    'See More',
-                    style: TextStyle(fontSize: 20, color: Colors.green),
+                Text(
+                  'Flutter Developer',
+                  style: TextStyle(
+                    fontSize: 32,
+                    fontWeight: FontWeight.w900,
+                    color: Colors.indigoAccent,
                   ),
                 ),
-                color: Colors.white,
-              ),
-            ],
+                SizedBox(
+                  height: 30,
+                ),
+                CircleAvatar(
+                  radius: 100,
+                  backgroundImage: AssetImage('img/sndmn.png'),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Text(
+                  'My name is Sochima',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.w700),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: <Widget>[
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Icon(
+                            Icons.mail_outline,
+                            size: 50,
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text(
+                            'bossan.sb@gmail.com',
+                            style: TextStyle(fontSize: 24),
+                          )
+                        ],
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Icon(
+                            Icons.location_on,
+                            size: 50,
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text(
+                            'Nigerian',
+                            style: TextStyle(fontSize: 24),
+                          )
+                        ],
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Icon(
+                            Icons.home,
+                            size: 50,
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text(
+                            'Flutter, Kotlin, Java',
+                            style: TextStyle(fontSize: 24),
+                          )
+                        ],
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Icon(
+                            Icons.account_circle,
+                            size: 50,
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text(
+                            'Experience Deisigner, Software Developer, Entreprenuer',
+                            style: TextStyle(fontSize: 24),
+                          )
+                        ],
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                    ],
+                  ),
+                ),
+                MaterialButton(
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (content) {
+                      return Details();
+                    }));
+                  },
+                  child: Padding(
+                    padding: EdgeInsets.only(top: 10, bottom: 10),
+                    child: Text(
+                      'See More',
+                      style: TextStyle(fontSize: 20, color: Colors.green),
+                    ),
+                  ),
+                  color: Colors.white,
+                ),
+              ],
+            ),
           ),
         ));
   }
